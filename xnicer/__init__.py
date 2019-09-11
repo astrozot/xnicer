9e94452c12a2b83701a13503c414ea6005c65ecd
Marco Lombardi <marco.lombardi@gmail.com>
2019-09-11 22:14:49 +0200

    Added xdecov imports

--- xnicer/__init__.py
+++ xnicer/__init__.py
@@ -4,6 +4,7 @@
 :Version: 0.1.0 of 2019/05/13"""
 
 from .xnicer import XNicer
+from .xdeconv import XD_Mixture, FIX_NONE, FIX_AMP, FIX_MEAN, FIX_COVAR, FIX_ALL
 from .catalogs import PhotometricCatalogue, ColorCatalogue, ExtinctionCatalogue
 from .kde import KDE
 from .mapping import guess_wcs, make_maps
